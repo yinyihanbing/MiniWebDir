f27eb28d7d76236292697a724f8f772459ddbfcf
李潘彬 <yinyihanbing@gmail.com>
2025-02-20 11:28:40 +0800

    no message

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var dir string
 var port int
 
 func main() {
-	flag.StringVar(&dir, "dir", "./", "指定靜態資源目錄")
+	flag.StringVar(&dir, "dir", "www", "指定靜態資源目錄")
 	flag.IntVar(&port, "port", 8181, "指定伺服器監聽的端口號")
 	flag.Parse()
 
@@ -22,6 +22,7 @@ func main() {
 
 	fs := http.FileServer(http.Dir(dir))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store")
 		fs.ServeHTTP(w, r)
 	})
 	log.Printf("伺服器已啟動, 端口:%d", port)
