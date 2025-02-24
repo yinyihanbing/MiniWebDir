7b60540f4853aa832e7387bdf505ca870007f5f3
李潘彬 <yinyihanbing@gmail.com>
2025-02-24 16:05:24 +0800

    no message

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+git pull
+
+docker builder prune
+
+docker build --no-cache -t ggweb .
+
+docker tag ggweb:latest yinyihanbing/ggweb:0.0.1
+
+docker rmi ggweb
+
+docker push yinyihanbing/ggweb:0.0.1
+
+cd ../dok
+
+docker-compose up -d ggweb
+
