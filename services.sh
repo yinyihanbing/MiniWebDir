3b6f2ea20c979c7ff3713e35657e121bbd7ab2e8
李潘彬 <yinyihanbing@gmail.com>
2025-02-27 16:23:45 +0800

    no message

--- services.sh
+++ services.sh
@@ -14,42 +14,42 @@ case $ACTION in
 			exit 0
 		fi
 
-		echo "[Unit]" | sudo tee ${SERVICE_FILE}
-		echo "Description=${PROJECT_NAME} service" | sudo tee -a ${SERVICE_FILE}
-		echo "After=network.target" | sudo tee -a ${SERVICE_FILE}
-		echo "[Service]" | sudo tee -a ${SERVICE_FILE}
-		echo "ExecStart=${CURRENT_DIR}/${BINARY_NAME}" | sudo tee -a ${SERVICE_FILE}
-		echo "Restart=always" | sudo tee -a ${SERVICE_FILE}
-		echo "User=jenkins" | sudo tee -a ${SERVICE_FILE}
-		echo "Group=jenkins" | sudo tee -a ${SERVICE_FILE}
-		echo "Environment=GO_ENV=production" | sudo tee -a ${SERVICE_FILE}
-		echo "WorkingDirectory=${CURRENT_DIR}/${BUILD_DIR}" | sudo tee -a ${SERVICE_FILE}
-		echo "[Install]" | sudo tee -a ${SERVICE_FILE}
-		echo "WantedBy=multi-user.target" | sudo tee -a ${SERVICE_FILE}
+		echo "[Unit]" | tee ${SERVICE_FILE}
+		echo "Description=${PROJECT_NAME} service" | tee -a ${SERVICE_FILE}
+		echo "After=network.target" | tee -a ${SERVICE_FILE}
+		echo "[Service]" | tee -a ${SERVICE_FILE}
+		echo "ExecStart=${CURRENT_DIR}/${BINARY_NAME}" | tee -a ${SERVICE_FILE}
+		echo "Restart=always" | tee -a ${SERVICE_FILE}
+		echo "User=jenkins" | tee -a ${SERVICE_FILE}
+		echo "Group=jenkins" | tee -a ${SERVICE_FILE}
+		echo "Environment=GO_ENV=production" | tee -a ${SERVICE_FILE}
+		echo "WorkingDirectory=${CURRENT_DIR}/${BUILD_DIR}" | tee -a ${SERVICE_FILE}
+		echo "[Install]" | tee -a ${SERVICE_FILE}
+		echo "WantedBy=multi-user.target" | tee -a ${SERVICE_FILE}
 
-		sudo systemctl daemon-reload
-		sudo systemctl enable ${PROJECT_NAME}
+		systemctl daemon-reload
+		systemctl enable ${PROJECT_NAME}
 		;;
 	uninstall)
 		if systemctl list-units --full -all | grep -Fq ${PROJECT_NAME}.service; then
-			sudo systemctl stop ${PROJECT_NAME}
-			sudo systemctl disable ${PROJECT_NAME}
-			sudo rm ${SERVICE_FILE}
-			sudo systemctl daemon-reload
+			systemctl stop ${PROJECT_NAME}
+			systemctl disable ${PROJECT_NAME}
+			rm ${SERVICE_FILE}
+			systemctl daemon-reload
 		else
 			echo "${PROJECT_NAME} service not found."
 		fi
-			;;
+		;;
 	start)
 		if systemctl list-units --full -all | grep -Fq ${PROJECT_NAME}.service; then
-			sudo systemctl start ${PROJECT_NAME}
+			systemctl start ${PROJECT_NAME}
 		else
 			echo "${PROJECT_NAME} service not found. Please install it first."
 		fi
 		;;
 	stop)
 		if systemctl list-units --full -all | grep -Fq ${PROJECT_NAME}.service; then
-			sudo systemctl stop ${PROJECT_NAME}
+			systemctl stop ${PROJECT_NAME}
 		else
 			echo "${PROJECT_NAME} service not found."
 		fi
